fe9df5ec39ac759d0791396e41f1321d50b44ecc
DenverNg <nhdduy67@gmail.com>
2024-12-13 00:44:59 +0700

    Done part 2

--- Project/Mininet/461_mininet/pox/part2controller.py
+++ Project/Mininet/461_mininet/pox/part2controller.py
@@ -1,10 +1,6 @@
-# Part 2 of UWCSE's Project 3
-#
-# based on Lab 4 from UCSC's Networking Class
-# which is based on of_tutorial by James McCauley
-
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
+import pox.lib.packet as pkt
 
 log = core.getLogger()
 
@@ -16,37 +12,53 @@ class Firewall(object):
     """
 
     def __init__(self, connection):
-        # Keep track of the connection to the switch so that we can
-        # send it messages!
+        # Keep track of the connection to the switch
         self.connection = connection
 
-        # This binds our PacketIn event listener
+        # Bind PacketIn event listener
         connection.addListeners(self)
 
-        # add switch rules here
+        # Add switch rules
+        # Allow ICMP traffic
+        msg_icmp = of.ofp_flow_mod()
+        msg_icmp.match = of.ofp_match(
+            dl_type=0x0800, nw_proto=1)  # IPv4 and ICMP
+        msg_icmp.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        msg_icmp.priority = 100
+        self.connection.send(msg_icmp)
+
+        # Allow ARP traffic
+        msg_arp = of.ofp_flow_mod()
+        msg_arp.match = of.ofp_match(dl_type=0x0806)  # ARP
+        msg_arp.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        msg_arp.priority = 100
+        self.connection.send(msg_arp)
+
+        # Default drop rule
+        msg_drop = of.ofp_flow_mod()
+        msg_drop.priority = 50  # Lower priority than ICMP/ARP rules
+        self.connection.send(msg_drop)
 
     def _handle_PacketIn(self, event):
         """
-        Packets not handled by the router rules will be
-        forwarded to this method to be handled by the controller
+        Packets not handled by switch rules will be
+        forwarded here to be handled by the controller.
         """
-
-        packet = event.parsed  # This is the parsed packet data.
+        packet = event.parsed
         if not packet.parsed:
             log.warning("Ignoring incomplete packet")
             return
 
-        packet_in = event.ofp  # The actual ofp_packet_in message.
-        print("Unhandled packet :" + str(packet.dump()))
+        # Debugging output
+        log.info("Unhandled packet: %s", str(packet.dump()))
 
 
 def launch():
     """
     Starts the component
     """
-
     def start_switch(event):
-        log.debug("Controlling %s" % (event.connection,))
+        log.debug("Controlling %s", event.connection)
         Firewall(event.connection)
 
     core.openflow.addListenerByName("ConnectionUp", start_switch)
